1ee1747eeba36971afe4cc67731c6d428341291a
Justin <justin@xano.com>
2022-09-23 17:34:03 -0700

    Clean lib folder

--- lib/xano-client.d.ts
+++
@@ -1,5 +0,0 @@
-import { BaseClient } from './base-client';
-export declare class XanoClient extends BaseClient {
-    protected getFormDataInstance(): any;
-}
-//# sourceMappingURL=xano-client.d.ts.map
--- lib/xano-node-client.d.ts
+++
@@ -1,5 +0,0 @@
-import { BaseClient } from './base-client';
-export declare class XanoNodeClient extends BaseClient {
-    protected getFormDataInstance(): any;
-}
-//# sourceMappingURL=xano-node-client.d.ts.map
