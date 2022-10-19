04b6cf381e997bce833499320dcebb0acf39f05b
Justin <justin@xano.com>
2022-10-19 10:23:47 -0700

    Fix nested body object in web client

--- lib/client.d.ts
+++ lib/client.d.ts
@@ -3,5 +3,6 @@ import { XanoClientConfig } from './interfaces/client-config';
 export declare class XanoClient extends XanoBaseClient {
     constructor(config: XanoClientConfig);
     protected getFormDataInstance(): any;
+    protected appendFormData(formData: any, key: string, value: any): void;
 }
 //# sourceMappingURL=client.d.ts.map
--- lib/node-client.d.ts
+++ lib/node-client.d.ts
@@ -1,5 +1,6 @@
 import { XanoBaseClient } from './base-client';
 export declare class XanoNodeClient extends XanoBaseClient {
     protected getFormDataInstance(): any;
+    protected appendFormData(formData: any, key: string, value: any): void;
 }
 //# sourceMappingURL=node-client.d.ts.map
