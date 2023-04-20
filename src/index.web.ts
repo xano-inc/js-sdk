f78737a8137a94ef01ad3dbebef2d592c5bf4e6b
Justin Albrecht <justin@xano.com>
2023-04-20 11:33:38 -0700

    Export storage classes to web SDK

--- src/index.web.ts
+++ src/index.web.ts
@@ -1,7 +1,22 @@
 import { XanoClient } from './client';
+import { XanoBaseStorage } from './models/base-storage';
+import { XanoCookieStorage } from './models/cookie-storage';
+import { XanoLocalStorage } from './models/local-storage';
+import { XanoObjectStorage } from './models/object-storage';
+import { XanoSessionStorage } from './models/session-storage';
 
 declare global {
+    var XanoBaseStorage: any;
     var XanoClient: any;
+    var XanoCookieStorage: any;
+    var XanoLocalStorage: any;
+    var XanoObjectStorage: any;
+    var XanoSessionStorage: any;
 }
 
+global.XanoBaseStorage = XanoBaseStorage;
 global.XanoClient = XanoClient;
+global.XanoCookieStorage = XanoCookieStorage;
+global.XanoLocalStorage = XanoLocalStorage;
+global.XanoObjectStorage = XanoObjectStorage;
+global.XanoSessionStorage = XanoSessionStorage;
